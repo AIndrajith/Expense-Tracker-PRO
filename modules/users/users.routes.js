13b8339289809d991ca71bb7cadf480f4dde6ea8
AIndrajith <deegalla12378@usci.ruh.ac.lk>
2025-09-20 10:58:54 +0530

    handling user logins

--- modules/users/users.routes.js
+++ modules/users/users.routes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const register = require("./Controllers/register");
+const login = require("./Controllers/login");
 
 const userRoutes = express.Router();
 
 // Routes...
 
 userRoutes.post("/register", register);
+userRoutes.post("/login", login);
 
 
 

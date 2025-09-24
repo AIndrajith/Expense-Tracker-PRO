f910d1193691415f4e50627336d2113b56c3c967
AIndrajith <deegalla12378@usci.ruh.ac.lk>
2025-09-24 10:31:48 +0530

    finalizing userDashboard

--- modules/users/Controllers/userDashboard.js
+++ modules/users/Controllers/userDashboard.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const userDashboard = async (req, res) => {
     const usersModel = mongoose.model("users");
+    const transactionModel = mongoose.model("transactions");
 
     console.log(req.user);
 
@@ -11,9 +12,17 @@ const userDashboard = async (req, res) => {
     //.select("name balance email") <- this part also do like this method
     .select("-password")  
 
+    const transactions = await transactionModel.find({
+        user_id: req.user._id,
+    })
+    .sort("createdAt")     // this is an acending order 
+    // if you want to get decending order you can add minus(-) simbol at the front like this (-createdAt)
+    .limit(3);
+
     res.status(200).json({
         status: "success",
         data: getUser,
+        transactions,  // transactions: transactions, <- this can be use like this 
     });
 
 };
